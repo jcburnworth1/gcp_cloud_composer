6f7c434aaef2bd1c4fa29fb0fb3c83ff6bdaebec
J.C. Burnworth <jcburnworth@JCs-MacBook-Pro.attlocal.net>
2021-12-27 12:24:28 -0600

    Adding dev / test conf/

--- utils/python/read_conf.py
+++ utils/python/read_conf.py
@@ -7,14 +7,21 @@ class read_conf():
 
     ## Initilize GCS client and get bucket details
     def __init__(self):
-        client = storage.Client()
-        self.client = client
+        self.client = storage.Client()
         self.bucket_name = os.environ['GCS_BUCKET']
-        self.bucket = client.get_bucket(os.environ['GCS_BUCKET'])
-        self.item = 'dags/conf/conf-prod.json'
+        self.bucket = self.client.get_bucket(os.environ['GCS_BUCKET'])
+        self.item = self.get_environ()
 
     def get_default_args(self):
         blob = self.bucket.blob(self.item)
         conf = json.loads(blob.download_as_string())
 
         return conf
+
+    def get_environ(self):
+        if self.bucket_name == 'us-central1-composer-burnwo-352245cf-bucket':
+            item = 'dags/conf/conf-prod.json'
+        else:
+            item = None
+
+        return item
