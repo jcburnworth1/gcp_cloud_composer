5ae989ee3be5dfd7227e901bdf9752f8cef60695
J.C. Burnworth <jcburnworth@JCs-MacBook-Pro.attlocal.net>
2022-01-08 22:14:53 -0600

    Setting up new dag script

--- tests/run_tests.sh
+++
@@ -1,3 +0,0 @@
-export AIRFLOW_HOME=/gcp_cloud_composer/
-cp */*_dag.py dag
-pytest
--- utils/bash/setup_new_dag_folder.sh
+++ utils/bash/setup_new_dag_folder.sh
@@ -1,11 +1,12 @@
 ## Setup new DAG directory with proper structure
 ## Must pass name of folder as only argument
 ## Naming Convention <dag_name>_pipeline
-## Example" taxi_data_pipeline
+## Example: taxi_data_pipeline
 folder=$1
 echo "Setting up new directory: $folder"
 
 mkdir $folder
-mkdir $folder"/sql"
-#touch $folder/"$folder""_pipeline_dag.py"
-#touch $folder/"$folder""_pipeline.md"
+cp -r template_pipeline/* $folder
+
+mv $folder/template_pipeline_dag.py $folder/$folder"_pipeline_dag.py"
+mv $folder/template_pipeline.md $folder/$folder"_pipeline.md"
